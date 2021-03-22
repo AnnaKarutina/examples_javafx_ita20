8577945c88cc004267965977766bd546614b2033
Anna Karutina <anna.karutina@khk.ee>
2021-03-22 18:38:17 +0200

    3.6 StackPane - taaned

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.layout.StackPane;
 
 public class Main extends Application{
@@ -20,8 +21,12 @@ public class Main extends Application{
 
         Label headerLbl = new Label("Press the login button");
         StackPane.setAlignment(headerLbl, Pos.TOP_CENTER);
+        StackPane.setMargin(headerLbl, new Insets(10, 0, 0, 0));
+
         Label statusLbl = new Label("Start screen");
         StackPane.setAlignment(statusLbl, Pos.BOTTOM_CENTER);
+        StackPane.setMargin(statusLbl, new Insets(0, 0, 10, 0));
+
         Button loginBtn = new Button("Login");
 
         StackPane root = new StackPane(headerLbl, statusLbl, loginBtn);
