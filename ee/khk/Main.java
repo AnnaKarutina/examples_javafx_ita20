0059d717ac6fa05b1c8e5790e7e07a7ca137b442
Anna Karutina <anna.karutina@khk.ee>
2021-03-22 08:06:28 +0200

    1.3.1 Scene objekti kasutamine - tühi akken

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -1,19 +1,28 @@
 package ee.khk;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     public static void main(String[] args) {
+
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) {
-        stage.setTitle("Hello JavaFX"); // akna pealkiri
-        stage.setWidth(250);            // akna laius
-        stage.setHeight(150);           // akna pikkus
-        stage.show();                   // akna kuvamine ekraanil
+
+        Group root = new Group();       // juursõlm
+        Scene scene = new Scene(root);  // Scene loomine
+        stage.setScene(scene);          // Scene määramine Stage
+
+        stage.setTitle("Hello JavaFX");
+        stage.setWidth(300);
+        stage.setHeight(150);
+
+        stage.show();
     }
 }
