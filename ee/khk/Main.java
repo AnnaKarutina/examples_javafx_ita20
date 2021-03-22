fbb6361fcd58f05191fe288c3a02edcf7b541174
Anna Karutina <anna.karutina@khk.ee>
2021-03-22 18:01:10 +0200

    3.4 VBox - nuppud ja label

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -3,6 +3,7 @@ package ee.khk;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.Priority;
@@ -17,11 +18,21 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) {
 
-        Button btn = new Button("Hello");
-        VBox vbox = new VBox();
-        VBox.setVgrow(btn, Priority.ALWAYS);
-        btn.setMaxHeight(Double.MAX_VALUE);
-        vbox.getChildren().addAll(btn);
+        Button okBtn = new Button("OK");
+        Button cancelBtn = new Button("Cancel");
+        Label lbl = new Label("Select");
+
+        VBox vbox = new VBox(15);
+
+        VBox.setVgrow(okBtn, Priority.ALWAYS);
+        okBtn.setMaxHeight(Double.MAX_VALUE);
+        //okBtn.setMaxWidth(100);
+
+        VBox.setVgrow(cancelBtn, Priority.ALWAYS);
+        cancelBtn.setMaxHeight(Double.MAX_VALUE);
+        //cancelBtn.setMaxWidth(100);
+
+        vbox.getChildren().addAll(lbl, okBtn, cancelBtn);
 
         Scene scene = new Scene(vbox, 300, 150);
         stage.setScene(scene);
