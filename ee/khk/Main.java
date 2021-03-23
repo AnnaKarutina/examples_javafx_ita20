69930302e0e0fb2f6f32debbb34fbc6fa59d0433
Anna Karutina <anna.karutina@khk.ee>
2021-03-23 21:36:06 +0200

    4.7 Slider

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -5,7 +5,8 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 
 import javafx.geometry.Orientation;
 
@@ -19,25 +20,24 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
 
-        Label headerLbl = new Label("ScrollPane");
-        Label textLbl = new Label("Lorem Ipsum is simply dummy text of the printing and typesetting \n" +
-                "industry. Lorem Ipsum has been the industry standard dummy \n" +
-                "text ever since the 1500s, when an unknown printer took a galley...");
+        Label lbl = new Label("Value");
 
-        ScrollPane scrollPane = new ScrollPane(textLbl);
-        scrollPane.setPrefViewportHeight(150);
-        scrollPane.setPrefViewportWidth(200);
+        Slider slider = new Slider(0.0, 20.0, 10.0);
+        slider.setShowTickMarks(true);
+        slider.setShowTickLabels(true);
+        slider.setBlockIncrement(2.0);
+        slider.setMajorTickUnit(5.0);
+        slider.setMinorTickCount(4);
+        slider.setSnapToTicks(true);
 
-        scrollPane.setPannable(false);
-        scrollPane.setVvalue(0.5);
-        scrollPane.setHvalue(0.5);
+        Button btn = new Button("Click");
+        btn.setOnAction(event -> lbl.setText("Slider Value: " + slider.getValue()));
 
-
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, headerLbl, scrollPane);
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, slider, lbl, btn);
         Scene scene = new Scene(root, 300, 250);
 
         stage.setScene(scene);
-        stage.setTitle("ScrollPane in JavaFX");
+        stage.setTitle("Slider in JavaFX");
         stage.show();
     }
 }
