a7b74dd9baaa5ec3d45a5304f72d4fe7d10e6c32
Anna Karutina <anna.karutina@khk.ee>
2021-03-23 23:28:51 +0200

    4.13 TableView - andmete kuvamine

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -5,9 +5,11 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tooltip;
-import javafx.geometry.Orientation;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Main extends Application{
 
@@ -19,17 +21,37 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
 
-        Label lbl = new Label("Label");
-        lbl.setTooltip(new Tooltip("This is a label"));
+        // loome nimekiri
+        ObservableList<Person> people = FXCollections.observableArrayList(
 
-        Button btn = new Button("Click");
-        btn.setTooltip(new Tooltip("Click the button \nto start an action"));
-        FlowPane root = new FlowPane(10, 10, btn, lbl);
+                new Person("Tom", 34),
+                new Person("Bob", 22),
+                new Person("Sam", 28),
+                new Person("Alice", 29)
+        );
+        // määrame tabel ja lisame andmed
+        TableView<Person> table = new TableView<Person>(people);
+        table.setPrefWidth(250);
+        table.setPrefHeight(200);
 
-        Scene scene = new Scene(root, 250, 200);
+        // veerg nime kuvamiseks
+        TableColumn<Person, String> nameColumn = new TableColumn<Person, String>("Name");
+        // määrame veeru vabrik, seose nime jargi
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("name"));
+        // lisame veerg
+        table.getColumns().add(nameColumn);
+
+        // veerg vanuse jaoks
+        TableColumn<Person, Integer> ageColumn = new TableColumn<Person, Integer>("Age");
+        ageColumn.setCellValueFactory(new PropertyValueFactory<Person, Integer>("age"));
+        table.getColumns().add(ageColumn);
+
+        FlowPane root = new FlowPane(10, 10, table);
+
+        Scene scene = new Scene(root, 300, 250);
 
         stage.setScene(scene);
-        stage.setTitle("Tooltip in JavaFX");
+        stage.setTitle("TableView in JavaFX");
         stage.show();
     }
 }
