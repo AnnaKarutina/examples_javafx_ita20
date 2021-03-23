689a013039496d91a6759127c66c98dc81a41b27
Anna Karutina <anna.karutina@khk.ee>
2021-03-23 22:26:39 +0200

    4.9 ComboBox - väljalangev nimekiri

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -4,8 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 import javafx.geometry.Orientation;
@@ -21,31 +21,20 @@ public class Main extends Application{
     public void start(Stage stage) throws Exception {
 
         ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
-        ListView<String> langsListView = new ListView<String>(langs);
-        langsListView.setPrefSize(250, 150);
+        ComboBox<String> langsComboBox = new ComboBox<String>(langs);
+        langsComboBox.setValue("Java"); // määrame vaikimisi valitud element
 
-        Button btn = new Button("Change");
+        Label lbl = new Label();
 
-        btn.setOnAction(event -> {
+        // saame valitud element
+        langsComboBox.setOnAction(event -> lbl.setText(langsComboBox.getValue()));
 
-            ObservableList<String> newLangs = FXCollections.observableArrayList("PHP", "Go", "C++");
-            langsListView.setItems(newLangs);
-        });
+        FlowPane root = new FlowPane(10, 10, langsComboBox, lbl);
 
-        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, btn, langsListView);
         Scene scene = new Scene(root, 300, 250);
 
         stage.setScene(scene);
-        stage.setTitle("ListView in JavaFX");
+        stage.setTitle("ComboBox in JavaFX");
         stage.show();
     }
 }
-
-
-
-
-
-
-
-
-
