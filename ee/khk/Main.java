c16221e688dc65f973c4fa879766b1b94f9d80d0
Anna Karutina <anna.karutina@khk.ee>
2021-03-23 21:57:35 +0200

    4.8 Nimekiri - valitud elemendi kontroll

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -9,6 +9,11 @@ import javafx.scene.control.ListView;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 
+import javafx.geometry.Orientation;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.ChangeListener;
+import javafx.scene.control.MultipleSelectionModel;
+
 public class Main extends Application{
 
     public static void main(String[] args) {
@@ -19,11 +24,23 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
 
-        // создаем список объектов
+        Label selectedLbl = new Label();
+        // loome nimekiri
         ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
         ListView<String> langsListView = new ListView<String>(langs);
+        langsListView.setPrefSize(250, 150);
+        // saame elementide valimise mudel
+        MultipleSelectionModel<String> langsSelectionModel = langsListView.getSelectionModel();
+        // määrame kuular muutuste jälgimiseks
+        langsSelectionModel.selectedItemProperty().addListener(new ChangeListener<String>(){
 
-        FlowPane root = new FlowPane(langsListView);
+            public void changed(ObservableValue<? extends String> changed, String oldValue, String newValue){
+
+                selectedLbl.setText("Selected: " + newValue);
+            }
+        });
+
+        FlowPane root = new FlowPane(Orientation.VERTICAL, 10, 10, selectedLbl, langsListView);
         Scene scene = new Scene(root, 250, 200);
 
         stage.setScene(scene);
