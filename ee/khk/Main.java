baada2c82e14891e1b67b14ba777c20b256e0cc7
Anna Karutina <anna.karutina@khk.ee>
2021-03-23 22:41:16 +0200

    4.11 TreeView

--- ee/khk/Main.java
+++ ee/khk/Main.java
@@ -4,10 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.control.Label;
-import javafx.scene.control.ChoiceBox;
-import javafx.collections.ObservableList;
-import javafx.collections.FXCollections;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.TreeItem;
 import javafx.geometry.Orientation;
 
 public class Main extends Application{
@@ -20,18 +18,32 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
 
-        ObservableList<String> langs = FXCollections.observableArrayList("Java", "JavaScript", "C#", "Python");
-        ChoiceBox<String> langsChoiceBox = new ChoiceBox<String>(langs);
-        langsChoiceBox.setValue("Java");
+        // määrame juurelement
+        TreeItem<String> rootTreeNode = new TreeItem<String>("Languages");
 
-        Label lbl = new Label();
-        langsChoiceBox.setOnAction(event -> lbl.setText(langsChoiceBox.getValue()));
-        FlowPane root = new FlowPane(10, 10, langsChoiceBox, lbl);
+        // määrame sisseehitatud elementide hulk - sõlmed
+        TreeItem<String> germanics = new TreeItem<String>("Germanic");
+        germanics.getChildren().add(new TreeItem<String>("German"));
+        germanics.getChildren().add(new TreeItem<String>("English"));
+
+        TreeItem<String> romans = new TreeItem<String>("Roman");
+        romans.getChildren().add(new TreeItem<String>("French"));
+        romans.getChildren().add(new TreeItem<String>("Spanish"));
+        romans.getChildren().add(new TreeItem<String>("Italian"));
+
+        // lisame sõlmed juurelemendi sisse
+        rootTreeNode.getChildren().add(germanics);
+        rootTreeNode.getChildren().add(romans);
+
+        // määrame TreeView jaoks juursõlm
+        TreeView<String> langsTreeView = new TreeView<String>(rootTreeNode);
+        langsTreeView.setPrefSize(150, 200);
+        FlowPane root = new FlowPane(10, 10, langsTreeView);
 
         Scene scene = new Scene(root, 300, 250);
 
         stage.setScene(scene);
-        stage.setTitle("ChoiceBox in JavaFX");
+        stage.setTitle("TreeView in JavaFX");
         stage.show();
     }
 }
